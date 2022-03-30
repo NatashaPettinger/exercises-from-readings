07cf80f57def57f778aa1a7882adf10ff7a9f0b2
NatashaPettinger <natasha.pettinger@gmail.com>
2022-03-30 11:02:49 -0600

    couple of short updates for javascript.info

--- javascriptDotInfo/4_5_ObjectsTheBasics_ConstructorOperatorNew.js
+++ javascriptDotInfo/4_5_ObjectsTheBasics_ConstructorOperatorNew.js
@@ -9,3 +9,27 @@ function B() { return obj; }
 alert( new A() == new B() ); // true
 
 //Create a new Calculator
+function Calculator() {
+
+    this.read = function (){
+        this.val1 = +prompt('First number:', 0);
+        this.val2 = +prompt('Second number:', '0');
+    };
+    this.sum = function() {
+        return this.val1 + this.val2
+    };
+    this.mul() = function() {
+        return this.val1 * this.val2
+    }
+}
+let calc = new Calculator();
+calc.read();
+
+//Create new Accumulator
+function Accumulator(value) {
+    currentValue = value;
+
+    this.read = function() {
+        this.value = this.value + prompt('New number:', 0);
+    };
+}
